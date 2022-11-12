01a614e241ffbf2d0ecb2adfb593ea551b4c7be2
nayooniverse <kangna99@naver.com>
2022-11-12 22:47:31 +0900

    feat(baseball) create skeleton code

---
+++ src/main/java/baseball/domain/NumberGenerator.java
@@ -0,0 +1,11 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class NumberGenerator {
+    public List<Integer> createRandomNumbers() {
+        return null;
+    }
+}
